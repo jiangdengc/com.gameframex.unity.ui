9208cdb4c0d6bb211f725edc0d39d4cdcee96b82
Blank <wangfj11@foxmail.com>
2024-08-29 18:20:44 +0800

    [增加]1. 增加初始化界面组的时候的根节点参数

--- Runtime/UI/IUIGroupHelper.cs
+++ Runtime/UI/IUIGroupHelper.cs
@@ -5,6 +5,8 @@
 // Feedback: mailto:ellan@gameframework.cn
 //------------------------------------------------------------
 
+using UnityEngine;
+
 namespace GameFrameX.UI.Runtime
 {
     /// <summary>
@@ -17,13 +19,14 @@ namespace GameFrameX.UI.Runtime
         /// </summary>
         /// <param name="depth">界面组深度。</param>
         void SetDepth(int depth);
-        
+
         /// <summary>
         /// 创建界面组。
         /// </summary>
+        /// <param name="root">根节点对象</param>
         /// <param name="groupName">界面组名称。</param>
         /// <param name="uiGroupHelperTypeName">界面组辅助器类型名。</param>
         /// <param name="customUIGroupHelper">自定义的界面组辅助器.</param>
-        IUIGroupHelper Handler(string groupName, string uiGroupHelperTypeName, IUIGroupHelper customUIGroupHelper);
+        IUIGroupHelper Handler(Transform root, string groupName, string uiGroupHelperTypeName, IUIGroupHelper customUIGroupHelper);
     }
 }
