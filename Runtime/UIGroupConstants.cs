b6b275b66c485ba44879e553da3aecde017c4a01
Blank <wangfj11@foxmail.com>
2024-08-29 10:06:48 +0800

    [增加]1. 增加UI组的预定义

---
+++ Runtime/UIGroupConstants.cs
@@ -0,0 +1,68 @@
+﻿namespace GameFrameX.UI.Runtime
+{
+    /// <summary>
+    /// 界面组常量
+    /// </summary>
+    public static class UIGroupConstants
+    {
+        /// <summary>
+        /// 隐藏
+        /// </summary>
+        public static UIGroupDefine Hidden = new UIGroupDefine(20, "Hidden");
+
+        /// <summary>
+        /// 底板
+        /// </summary>
+        public static UIGroupDefine Floor = new UIGroupDefine(15, "Floor");
+
+        /// <summary>
+        /// 正常
+        /// </summary>
+        public static UIGroupDefine Normal = new UIGroupDefine(10, "Normal");
+
+        /// <summary>
+        /// 固定
+        /// </summary>
+        public static UIGroupDefine Fixed = new UIGroupDefine(0, "Fixed");
+
+        /// <summary>
+        /// 窗口
+        /// </summary>
+        public static UIGroupDefine Window = new UIGroupDefine(-10, "Window");
+
+        /// <summary>
+        /// 提示
+        /// </summary>
+        public static UIGroupDefine Tip = new UIGroupDefine(-15, "Tip");
+
+        /// <summary>
+        /// 引导
+        /// </summary>
+        public static UIGroupDefine Guide = new UIGroupDefine(-20, "Guide");
+
+        /// <summary>
+        /// 引导
+        /// </summary>
+        public static UIGroupDefine BlackBoard = new UIGroupDefine(-22, "BlackBoard");
+
+        /// <summary>
+        /// 引导
+        /// </summary>
+        public static UIGroupDefine Dialogue = new UIGroupDefine(-23, "Dialogue");
+
+        /// <summary>
+        /// Loading 
+        /// </summary>
+        public static UIGroupDefine Loading = new UIGroupDefine(-25, "Loading");
+
+        /// <summary>
+        /// 通知
+        /// </summary>
+        public static UIGroupDefine Notify = new UIGroupDefine(-30, "Notify");
+
+        /// <summary>
+        /// 系统顶级
+        /// </summary>
+        public static UIGroupDefine System = new UIGroupDefine(-35, "System");
+    }
+}
