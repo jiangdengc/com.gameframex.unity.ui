be6edcc10fa3fa10a7a54065bf7a4f8588cc6d01
Blank <wangfj11@foxmail.com>
2024-08-29 18:17:04 +0800

    [增加]1. 增加UI事件监听器

---
+++ Runtime/UIEventSubscriber.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using GameFrameX.Event.Runtime;
+using GameFrameX.Runtime;
+
+namespace GameFrameX.UI.Runtime
+{
+    /// <summary>
+    /// UI事件订阅器
+    /// </summary>
+    public sealed class UIEventSubscriber : IReference
+    {
+        private readonly GameFrameworkMultiDictionary<string, EventHandler<GameEventArgs>> m_DicEventHandler = new GameFrameworkMultiDictionary<string, EventHandler<GameEventArgs>>();
+
+        /// <summary>
+        /// 持有者
+        /// </summary>
+        public object Owner { get; private set; }
+
+        public UIEventSubscriber()
+        {
+            m_DicEventHandler = new GameFrameworkMultiDictionary<string, EventHandler<GameEventArgs>>();
+            Owner = null;
+        }
+
+        /// <summary>
+        /// 订阅
+        /// </summary>
+        /// <param name="id">消息ID</param>
+        /// <param name="handler">处理对象</param>
+        /// <exception cref="Exception"></exception>
+        public void Subscribe(string id, EventHandler<GameEventArgs> handler)
+        {
+            if (handler == null)
+            {
+                throw new Exception("Event handler is invalid.");
+            }
+
+            m_DicEventHandler.Add(id, handler);
+            GameEntry.GetComponent<EventComponent>().Subscribe(id, handler);
+        }
+
+        /// <summary>
+        /// 取消订阅
+        /// </summary>
+        /// <param name="id">消息ID</param>
+        /// <param name="handler">处理对象</param>
+        /// <exception cref="Exception"></exception>
+        public void UnSubscribe(string id, EventHandler<GameEventArgs> handler)
+        {
+            if (!m_DicEventHandler.Remove(id, handler))
+            {
+                throw new Exception(Utility.Text.Format("Event '{0}' not exists specified handler.", id.ToString()));
+            }
+
+            GameEntry.GetComponent<EventComponent>().Unsubscribe(id, handler);
+        }
+
+        /// <summary>
+        /// 取消所有订阅
+        /// </summary>
+        public void UnSubscribeAll()
+        {
+            if (m_DicEventHandler == null)
+            {
+                return;
+            }
+
+            foreach (var item in m_DicEventHandler)
+            {
+                foreach (var eventHandler in item.Value)
+                {
+                    GameEntry.GetComponent<EventComponent>().Unsubscribe(item.Key, eventHandler);
+                }
+            }
+
+            m_DicEventHandler.Clear();
+        }
+
+        /// <summary>
+        /// 创建事件订阅器
+        /// </summary>
+        /// <param name="owner">持有者</param>
+        /// <returns></returns>
+        public static UIEventSubscriber Create(object owner)
+        {
+            var eventSubscriber = ReferencePool.Acquire<UIEventSubscriber>();
+            eventSubscriber.Owner = owner;
+
+            return eventSubscriber;
+        }
+
+        /// <summary>
+        /// 清理
+        /// </summary>
+        public void Clear()
+        {
+            m_DicEventHandler.Clear();
+            Owner = null;
+        }
+    }
+}
